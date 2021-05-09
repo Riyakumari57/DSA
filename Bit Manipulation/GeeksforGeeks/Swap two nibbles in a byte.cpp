aeb48d584697d19d887758b4899a4c512cf715ce
Riya Kumari <65845230+Riyakumari57@users.noreply.github.com>
2021-05-09 13:42:12 +0530

    Update Swap two nibbles in a byte.cpp

--- Bit Manipulation/GeeksforGeeks/Swap two nibbles in a byte.cpp
+++ Bit Manipulation/GeeksforGeeks/Swap two nibbles in a byte.cpp
@@ -4,6 +4,8 @@ public:
     {
         // code here
         x = (((x & 0x0F)<<4) | ((x & 0xF0)>>4));
+        /*The expression “x & 0x0F” gives us the last 4 bits of data and The expression “x & 0xF0” gives us first four bits of data
+        */
         return x;
     }
 };
