ea0f14b49cb2e8fc81e28f47ced47d397700025b
Riya Kumari <65845230+Riyakumari57@users.noreply.github.com>
2021-05-25 10:00:55 +0530

    Create Factorial.cpp

---
+++ Recursion/Recursion Basics/Factorial.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fact(int n)
+{
+    if(n==1)
+    {
+        return n;
+    }
+    return n*fact(n-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fact(n);
+}
