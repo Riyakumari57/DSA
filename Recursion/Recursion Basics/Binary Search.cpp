21929afc0ca48c8c3a193a393e55201fccbb2748
Riya Kumari <65845230+Riyakumari57@users.noreply.github.com>
2021-05-26 20:07:57 +0530

    Update Binary Search.cpp

--- Recursion/Recursion Basics/Binary Search.cpp
+++ Recursion/Recursion Basics/Binary Search.cpp
@@ -2,38 +2,26 @@
 using namespace std;
 int BinarySearch(int a[], int k, int n, int ele)
 {
-    int f=0,mid;
-    while (k <= n)
-    {
-        mid = (k + n) / 2;
+    int f=0;
+       int  mid = (k + n) / 2;
         if (ele < a[mid])
         {
-            n=mid-1;
+         return  BinarySearch(a, 0, mid-1, ele);
         }
         else if (ele > a[mid])
         {
-            k=mid+1;
+          return  BinarySearch(a, mid+1, n, ele);
         }
         else if (ele == a[mid])
         {
-            f=1;
-            break;
+            return mid;
         }
-    }
-    if(f!=0)
-    {
-        cout << "Found at location " << mid << endl;
-    }
-    else
-    {
-        cout<<"Not Found"<<endl;
-    }
 }
 int main()
 {
     int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int n = sizeof(a) / sizeof(a[0]);
-    int element;
-    cin >> element;
-    BinarySearch(a, 0, n - 1, element);
+    int ele;
+    cin >> ele;
+    cout<<BinarySearch(a, 0, n - 1, ele);
 }
