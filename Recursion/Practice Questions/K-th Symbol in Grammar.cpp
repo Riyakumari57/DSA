a4a21b16f7a0291df3223a064e0caa6e1cafaed2
Riya Kumari <65845230+Riyakumari57@users.noreply.github.com>
2021-05-27 13:31:56 +0530

    Create K-th Symbol in Grammar.cpp

---
+++ Recursion/Practice Questions/K-th Symbol in Grammar.cpp
@@ -0,0 +1,18 @@
+//non recursive approach
+
+#include<bits/stdc++.h>
+using namespace std;
+int kthGrammar(int n, int k)
+{
+    if(n==1 && k==1) return 0;
+    int mid = pow(2,n-1)/2;
+    if(k<=mid) return kthGrammar(n-1,k);
+    else return !(kthGrammar(n-1,k-mid)); 
+}
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    cout<<kthGrammar(n,k)<<endl;
+    
+}
